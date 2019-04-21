b2b85d02d0654a41cfb44578212a81a9f36f90ab
Thinker222 <es0197@yahoo.com>
2019-04-21 01:59:28 -0400

    Added add_all
    - Added add_all

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # This a test file for cloud computing. 
+def add_all(x):
+    if x <= 1:
+        return x
+    else:
+        return x + add_all(x - 1)
 
 def main():
     x = 2
     y = 4
     z = 3
     print((x + y) / z)
-    v = [var * var for var in range(100) if var % 2 == 0]
+    v = [add_all(var) for var in range(100) if var % 2 == 0]
     print(v)
-
+   
 if '__main__':
     main()
